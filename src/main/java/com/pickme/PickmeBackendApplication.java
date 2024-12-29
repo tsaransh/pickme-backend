af71647a9992118fe2021a14b826c9abe4cdbaef
Saransh Tyagi <tyagisaransh90@gmail.com>
2024-12-29 11:56:18 +0530

    Project setup

---
+++ src/main/java/com/pickme/PickmeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pickme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickmeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickmeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pickme/PickmeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickmeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
