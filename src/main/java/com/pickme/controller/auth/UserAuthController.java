fff2d5edb63e350c597aa8280370b00ce694c7a9
Saransh Tyagi <tyagisaransh90@gmail.com>
2024-12-29 12:12:02 +0530

    add auth controller and dependencies

---
+++ src/main/java/com/pickme/controller/auth/UserAuthController.java
@@ -0,0 +1,20 @@
+package com.pickme.controller.auth;
+
+import com.pickme.payloads.UserCredentials;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth/user")
+public class UserAuthController {
+
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticateUser(@RequestBody UserCredentials credentials) {
+       return new ResponseEntity<>("Login", HttpStatus.OK);
+    }
+
+}
