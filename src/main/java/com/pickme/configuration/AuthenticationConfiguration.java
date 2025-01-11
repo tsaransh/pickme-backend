d681ddd3a295f7c5684e840c2ea0848873b74d64
Saransh Tyagi <tyagisaransh90@gmail.com>
2025-01-11 16:01:05 +0530

    Auth controller added

--- src/main/java/com/pickme/configuration/AuthenticationConfiguration.java
+++ src/main/java/com/pickme/configuration/AuthenticationConfiguration.java
@@ -1,7 +1,18 @@
 package com.pickme.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AuthenticationConfiguration {
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
